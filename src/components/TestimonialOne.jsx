233d80e799c9e59ff8ac6a92a5653fd1c4a9edcb
Angel Gonzalez <angelgonzalez2118@gmail.com>
2021-08-20 16:30:19 -0400

    create-react-app for Heroku

--- src/components/TestimonialOne.jsx
+++ src/components/TestimonialOne.jsx
@@ -15,22 +15,12 @@ export default function TestimonialOne(props) {
         </div>
         <div className="testimonial__info gray bold">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu
             ipsum non sapien lacinia pulvinar. Nulla lacus tellus, dapibus et
             tristique quis, porttitor sit amet sem. Ut turpis quam, feugiat sit
             amet viverra iaculis, vehicula id elit. Sed lectus est, laoreet vel
             malesuada sed, suscipit ut nibh. Praesent consectetur elit vitae
-            dolor vehicula gravida. Duis volutpat velit eget purus dapibus, et
-            eleifend velit faucibus. Etiam ullamcorper rhoncus tempus. Fusce
-            euismod et nibh id pharetra. Curabitur ac venenatis mauris, et
-            vulputate velit. Maecenas ut est ligula. Fusce et ipsum diam.
-            Interdum et malesuada fames ac ante ipsum primis in faucibus.
-            Aliquam erat volutpat. Donec egestas sit amet sem non viverra.
-            Pellentesque magna est, dapibus in purus pulvinar, malesuada commodo
-            ante. Curabitur suscipit sagittis tortor, id dapibus justo luctus
-            at. Quisque pulvinar consequat dictum. Sed sagittis urna nunc, quis
-            cursus massa euismod et. Sed eu sem dui. Donec ut tempor dui. Morbi
-            turpis ex, convallis hendrerit augue a, lacinia condimentum lorem.
+            dolor vehicula gravida. Duis volutpat velit eget purus dapibus.
           </p>
         </div>
         <div className={`testimonial__video ${props.rightVideo}`}>
